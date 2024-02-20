e175f3eda68fb5088fbc5f989618eb814bc4dd40
philip ellis <philip.ellis.j@gmail.com>
2024-02-20 00:53:52 -0500

    bumped version

--- Communication.cpp
+++ Communication.cpp
@@ -142,7 +142,7 @@ void Communication::sendAdmin() {
       admin = 0;
       break;
     case VERSION:
-      Serial.print(F("V,1.13.0\r\n"));
+      Serial.print(F("V,1.15.0\r\n"));
       admin = 0;
       break;
     }
